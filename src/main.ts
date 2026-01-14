04eac177b5504480b9d25e36331a6165c5342b17
sand02004 <s.mugwaneza@alustudent.com>
2026-01-14 07:18:25 -0800

    chore: adding swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import dotenv from 'dotenv';
 dotenv.config();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setVersion('1.0.0')
+  .setTitle('Women Connect Rwanda API')
+  .addTag('User')
+  .addTag('Profiles')
+  .addTag('Opportunities')
+  .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
-  const port=process.env.PORT ;
+  const port = process.env.PORT;
   console.log(`application is running on: http:/localhost:${port}`);
+  console.log(`swagger is running on port: http:localhost:${port}/api`)
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, Validation
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
